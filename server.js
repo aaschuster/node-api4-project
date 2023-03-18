4406f86e4435e41d6b6fdd8bd6c7ea14f43b4fca
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-18 18:23:25 -0400

    add in database

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const server = express();
 
 server.use(express.json());
 
+const data = require("./data.json");
+
 server.get("/", (req, res) => {
     res.send("This is the root response");
+})
+
+server.get("/api/users", (req, res) => {
+    res.json(data);
 })
 
 module.exports = server;
